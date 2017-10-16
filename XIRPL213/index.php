728290b294a81cf44cd0538256a1991d327b96bb
Endromuhammad <30395565+Endromuhammad@users.noreply.github.com>
2017-10-16 07:19:10 +0700

    Add files via upload

---
+++ XIRPL213/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+ <title>Instagram</title>
+ 
+ <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ <form action="view.php" method="post">
+ <h2><i class="fa fa-user"></i>Instagram</h2>
+ <h3>Sign up to see photos and videos from your friends.</h3>
+ <br/>
+ <br>
+ <div class="form-group">
+  <button class="btn btn-submit" type="submit">Log In With Facebook &nbsp; <i class="fa fa-hand-o-right" aria-hidden="true"></i></button>
+ </div>
+ <br/>
+ <br>
+ <h4>------------------   OR   ------------------</h4>
+ <br/>
+ <div class="form-group">
+  <input type="text" class="input-mode" placeholder="Mobile Number or Email" name="email" id="email">
+ </div>
+ <div class="form-group">
+ <input type="text" class="input-mode" placeholder="Nama lengkap" name="lengkap" id = "lengkap">
+ </div>
+ <div class="form-group">
+ <input type="text" class="input-mode" id ="pengguna" name="pengguna" placeholder="Nama Pengguna"> 
+ </div>
+  <div class="form-group">
+ <input type="password" class="input-mode"  placeholder="Kata Sandi" > 
+ </div>
+ <br/>
+ <div class="form-group">
+  <button class="btn btn-submit" type="submit">Daftar &nbsp; <i class="fa fa-hand-o-right" aria-hidden="true"></i></button>
+ </div>
+ <br/>
+ <br>
+<h5> By signing up, you agree to our Terms & Privacy Policy.</h5>
+<br/>
+ </form>
+</body>
+</html>
---
+++ XIRPL213/view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pendaftaran berhasil</title>
+</head>
+<body>
+<p align="center"><font size="5px"><b>SIGNUP SUCCESS</b></font></p>
+	<table align="center">
+		<tr>
+			<td>Mobile Number or Email</td><td>:</td><td><?php echo $_POST["email"]; ?></td>
+		</tr>
+		<tr>
+			<td>Nama Lengkap</td><td>:</td><td><?php echo $_POST["lengkap"]; ?></td>
+		</tr>
+		<tr>
+			<td>Nama Pengguna</td><td>:</td><td><?php echo $_POST["pengguna"]; ?></td>
+		</tr>
+
+		</table>
+</body>
+</html>
